79c1f48181a5785fa4bc2c3f9d27af3a9c1ebb22
SanketTambe39 <sankettambe39@gmail.com>
2021-08-31 22:34:52 +0530

    ADDED: Initial Files

---
+++ src/test/java/com/example/springgreetingapplication/SpringGreetingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springgreetingapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGreetingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
